6d28e0ca631d4e61853355b9a70e2466be32d355
ThandarPhyoe <thandarthuphyoe@gmail.com>
2018-06-08 02:09:50 -0700

    ex7.py

---
+++ ex7.py
@@ -0,0 +1,6 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
