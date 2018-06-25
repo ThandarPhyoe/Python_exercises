e627de6d2e3ca5368a078954722f000fc12b066c
LwinMeMeKhaing.github.io <www.lwinmeme@gmail.com>
2018-06-25 17:55:33 +0530

    ex9.py

--- ex9.py
+++ ex9.py
@@ -1,10 +1,8 @@
-days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-print("Here are the days: ", days)
-print("Here are the months: ",months)
-print("""
-There's something going on here.
-With the three double-quotes.
-We'll be able to type as much as we like.
-Even 4 lines if we want, or 5, or 6.
-""")
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+print("Here are the days:",days)
+print("Here are the months:",months)
+print("There's something going on here.")
+print("With the three doouble-quotes.")
+print("We'll be able to type as much as we like.")
+print("Even 4 lines if we want, or 5, or 6.")
