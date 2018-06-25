f16c484e6ba55abb254f0e0bde3e794af0159352
LwinMeMeKhaing.github.io <www.lwinmeme@gmail.com>
2018-06-25 18:04:43 +0530

    ex11.py

--- ex11.py
+++ ex11.py
@@ -1,7 +1,7 @@
-print("How old are you?", end=' ')
-age = input()
-print("How tall are you?", end=' ')
-height = input()
-print("How much do you weight?", end=' ')
-weight = input()
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print("How old are you?"),
+age=input()
+print("How tall are you"),
+height=input()
+print("How much do you weigh"),
+weight=input()
+print("So, you're %r old,%r tall and %r heavy."%(age,height,weight))
